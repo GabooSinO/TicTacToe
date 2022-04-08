7cb8318a97a2cb052407c1df17c32b1421d9f3e5
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-08 17:14:41 +0200

    Cleaned code and added tie thing

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,14 +1,11 @@
-# 0 -empy
+# 0 - empy
 # 1 - player1
 # 2 - player2
-import sys
-
 state = [ 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 #--------------------------------FUNTIONS---------------------------------------
 
 def print_state(state):
-    global a
     a = []
     for item in state:
         if item == 0:
@@ -18,82 +15,71 @@ def print_state(state):
         elif item == 2:
             a.append("O")
 
-    return
-
-def print_map():
     print(f"|{a[0]}|{a[1]}|{a[2]}|")
     print(f"|{a[3]}|{a[4]}|{a[5]}|")
     print(f"|{a[6]}|{a[7]}|{a[8]}|")
 
-    return
-
 def turn_p1():
-    global player1
     while True:
-        player1 = int(input("Player One chooses position(1-9): "))
-        if player1 == 1 and state[0] == 0 and state[0] != 2:
-            state[0] = 1
-        elif player1 == 2 and state[1] == 0 and state[1] != 2:
-            state[1] = 1
-        elif player1 == 3 and state[2] == 0 and state[2] != 2:
-            state[2] = 1
-        elif player1 == 4 and state[3] == 0 and state[3] != 2:
-            state[3] = 1
-        elif player1 == 5 and state[4] == 0 and state[4] != 2:
-            state[4] = 1
-        elif player1 == 6 and state[5] == 0 and state[5] != 2:
-            state[5] = 1
-        elif player1 == 7 and state[6] == 0 and state[6] != 2:
-            state[6] = 1
-        elif player1 == 8 and state[7] == 0 and state[7] != 2:
-            state[7] = 1
-        elif player1 == 9 and state[8] == 0 and state[8] != 2:
-            state[8] = 1
+        try:
+            player1 = int(input("Player One chooses position(1-9): "))
+        except Exception:
+            continue    # If it cant convert it to int, ask again
+
+        # Check if the player's input is out of range
+        if player1 not in range(1,10):
+            print("Wrong input! Try again...")
+            continue
+
+        # Set the cell state if the cell is empty
+        if state[player1-1] == 0:
+            state[player1-1] = 1
         else:
-            player1 = int(player1)
+            print(f"Place {player1} is taken by Player {state[player1-1]}! Try again...")
+            continue
 
-        if player1 in range(1,10):
-            break
-
-    print_state(state)
-    print_map()
-
-    return
+        print_state(state)
+        if check_win():
+            print("Player 1 wins!")
+            return True     # Wins
+        return False
 
 def turn_p2():
-    global player2
     while True:
-        player2 = int(input("Player Two chooses position(1-9): "))
-        if player2 == 1 and state[0] == 0 and state[0] != 1:
-            state[0] = 2
-        elif player2 == 2 and state[1] == 0 and state[1] != 1:
-            state[1] = 2
-        elif player2 == 3 and state[2] == 0 and state[2] != 1:
-            state[2] = 2
-        elif player2 == 4 and state[3] == 0 and state[3] != 1:
-            state[3] = 2
-        elif player2 == 5 and state[4] == 0 and state[4] != 1:
-            state[4] = 2
-        elif player2 == 6 and state[5] == 0 and state[5] != 1:
-            state[5] = 2
-        elif player2 == 7 and state[6] == 0 and state[6] != 1:
-            state[6] = 2
-        elif player2 == 8 and state[7] == 0 and state[7] != 1:
-            state[7] = 2
-        elif player2 == 9 and state[8] == 0 and state[8] != 1:
-            state[8] = 2
+        try:
+            player2 = int(input("Player Two chooses position(1-9): "))
+        except Exception:
+            continue
+
+        if player2 not in range(1,10):
+            print("Wrong input! Try again...")
+            continue
+
+        if state[player2-1] == 0:
+            state[player2-1] = 2
         else:
-            player2 = int(player2)
+            print(f"Place {player2} is taken by Player {state[player2-1]}! Try again...")
+            continue
 
-        if player2 in range(1,10):
-            break
+        print_state(state)
+        if check_win():
+            print("Player 2 wins!")
+            return True
+        return False
 
-    print_state(state)
-    print_map()
+def check_win():
+    # You only need to check the cross
+    for n in state:
+        if (n in [1,4,7]) and (state[n-1] == state[n] == state[n+1]):       # Horizontal
+            return True
+        elif (n in [3,4,5]) and (state[n-3] == state[n] == state[n+3]):     # Vertical
+            return True
+        elif (n == 4) and (state[0] == state[n] == state[8] or state[2] == state[n] == state[6]):                               # Diagonal
+            return True
+        else:
+            return False
 
-    return
-
-def win():
+    """
     if ((state[0]==state[1]==state[2]) and state[0]== 1) or (state[3]==state[4]==state[5] and state[3]== 1) or ((state[6]==state[7]==state[8]) and state[6]== 1):
         return 1
     elif ((state[0]==state[1]==state[2]) and state[0]== 2 ) or ((state[3]==state[4]==state[5]) and state[3]== 2) or ((state[6]==state[7]==state[8]) and state[6]== 2):
@@ -108,26 +94,28 @@ def win():
         return 2
     else:
         return 0
+    """
 
-#------------------------------GAME---------------------------------------------
+#------------------------------------GAME------------------------------------------
 
 while True:
+    count = 0   # Count the number of steps
     while True:
-        turn_p1()
-
-        if win() == 1:
-            print("Player One wins!")
+        if count < 8:
+            if turn_p1():   # P1 wins
+                break
+        if count < 9:
+            if turn_p2():   # P2 wins
+                break
+        else:
+            print("Tie!")
             break
-
-        turn_p2()
-
-        if win() == 2:
-            print("Player Two wins!")
-            break
+        count += 2          # 2 turns / loop
 
     finish = int(input("Game finished, choose 1 to play another game, or 2 to exit: "))
     if finish == 1:
         state = [ 0, 0, 0, 0, 0, 0, 0, 0, 0] 
         print("Starting new game...")
-    elif finish == 2:
-        sys.exit("Leaving the game...")
+    else:
+        exit("Leaving the game...")
+
